a95986e6c04dd513fd8f5f2b82bc584ce8cf54d2
Nick-theMak <nicholasmakdessi2002@gmail.com>
2024-02-19 17:49:33 +1100

    initialise

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <header className="App-header">Content
+      <hr />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
